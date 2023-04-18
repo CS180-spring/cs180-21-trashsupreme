1d4897753b6d0b740485231efdb670fb7935a04b
Justin Sanders <justin.h.sanders93@gmail.com>
2023-04-18 13:20:21 -0700

    Initial draft of FileNode with all written methods. (Untested)

--- FileNode.h
+++ FileNode.h
@@ -2,13 +2,49 @@
 #define _FILENODE_H_
 
 #include <ctime>
+#include <iostream>
 
 class FileNode {
 public:
-	FileNode() {}
-	~FileNode() {}
-	char* get_filename() {}
-	std::time_t get_timestamp() {}
+	FileNode(char* fn) : //Receive pointer to filename string
+		//Store filename and retrieve timestamp as time since Epoch (Avoid locality time differences)
+		filename(fn), timestamp(std::time(nullptr)) {} 
+	
+	//Filename string should be dynamically allocated at runtime so memory should be freed when file is removed from Database
+	~FileNode() {
+		delete[] filename; 
+	}
+
+	//Return the pointer to the filename
+	char* get_Filename() {
+		return filename;
+	}
+
+	//Return time since last Epoch
+	std::time_t get_Timestamp() {
+		return timestamp;
+	}
+
+	//Print Filename and Date in Local Calendar Format
+	void printFile() {
+		std::cout << "File Name: " << filename
+			<< "\n Date Last Updated: " << std::asctime(std::localtime(&timestamp)) << std::endl;
+	}
+
+	//Change Filename to provided Filename and print status of update
+	//Currently assumes file extension is passed as part of file name string so no parsing occurs here)
+	//Updates timestamp once update completes
+	void updateFilename(char* fn) {
+		std::cout << "Updating File Name...\n"
+			<< "\tCurrent File Name: " << filename
+			<< "\n\tNew File Name: " << fn;
+
+		delete[] filename;
+		filename = fn;
+
+		std::cout << "\n\n Update Complete!";
+	}
+
 
 private:
 	char* filename;
