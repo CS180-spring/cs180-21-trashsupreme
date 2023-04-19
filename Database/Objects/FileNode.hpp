6a814b244d9b688d9b77b4ea3a70e84891e8fe24
Justin Sanders <justin.h.sanders93@gmail.com>
2023-04-18 19:46:01 -0700

    Created Unit Tests for FileNode Object

--- Database/Objects/FileNode.hpp
+++ Database/Objects/FileNode.hpp
@@ -8,7 +8,7 @@ class FileNode {
 public:
 	FileNode(char* fn) : //Receive pointer to filename string
 		//Store filename and retrieve timestamp as time since Epoch (Avoid locality time differences)
-		filename(fn), timestamp(std::time(nullptr)) {} 
+		filename(fn), timestamp(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())) {} 
 	
 	//Filename string should be dynamically allocated at runtime so memory should be freed when file is removed from Database
 	~FileNode() { 
@@ -27,21 +27,15 @@ public:
 	//Print Filename and Date in Local Calendar Format
 	void printFile() {
 		std::cout << "File Name: " << filename
-			<< "\n Date Last Updated: " << std::asctime(std::localtime(&timestamp)) << std::endl;
+			<< "\nDate Last Updated: " << std::asctime(std::localtime(&timestamp)) << std::endl;
 	}
 
 	//Change Filename to provided Filename and print status of update
 	//Currently assumes file extension is passed as part of file name string so no parsing occurs here)
 	//Updates timestamp once update completes
 	void updateFilename(char* fn) {
-		std::cout << "Updating File Name...\n"
-			<< "\tCurrent File Name: " << filename
-			<< "\n\tNew File Name: " << fn;
-
-		delete[] filename;
 		filename = fn;
-
-		std::cout << "\n\n Update Complete!";
+		timestamp = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
 	}
 
 
