283ef62fcc36f0a13e5936a53c8286fb6db5bb90
Jacob-Cunningham <jacob.s.cunningham@gmail.com>
2023-04-23 16:45:58 -0700

    Created NavBar HTML
    Created NavBar SFC which takes an array labels which has a string for each button on the bar. It emits clicked with an additional string that is the button that was clicked.
    Created SFC for Login, MyFiles, and Search to be implemented.

---
+++ Trash_Bin/src/components/MyFiles.vue
@@ -0,0 +1,10 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+    export default defineComponent ({
+        
+    })
+</script>
+
+<template>
+    <h2>My Files Menu</h2>
+</template>
