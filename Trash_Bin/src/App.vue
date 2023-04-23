61413888546cc58b127a0215f4573115625ca474
hscottvo <56327086+hscottvo@users.noreply.github.com>
2023-04-23 12:55:04 -0700

    Added json requests template code

--- Trash_Bin/src/App.vue
+++ Trash_Bin/src/App.vue
@@ -1,19 +1,23 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+import SendButton from "./components/SendButton.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div id="test">
+      <SendButton />
+      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
     </div>
+    <!-- <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div> -->
   </header>
 
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    <SendButton />
   </main>
 </template>
 
