c96d9a5e1b08e59fb23e105f1e0032c55395c59f
Jacob-Cunningham <jacob.s.cunningham@gmail.com>
2023-04-24 00:11:15 -0700

    Add File Browser
    Added structure for items and folders.
    Items and folders can be deleted by clicking the minus symbol.
    New folders can be added by clicking the add symbol of their parent folder.
    Clicking on a folder opens or closes it.

--- Trash_Bin/src/components/MyFiles.vue
+++ Trash_Bin/src/components/MyFiles.vue
@@ -1,10 +1,21 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
-    export default defineComponent ({
-        
+import Folder from './MyFiles_Components/Folder.vue'
+//import tree from './MyFiles_Components/testFileTree.json'
+import {Types} from '../types'
+export default defineComponent ({
+        components: {
+            Folder
+        },
+        methods: {
+            constructFileTree() {
+                return new Types.Folder("root", [new Types.Folder("Folder1", [], [new Types.Item("File2", "20MB")])], [new Types.Item("File1", "3GB")])
+            }
+        }
     })
 </script>
 
 <template>
     <h2>My Files Menu</h2>
+    <Folder :folder="constructFileTree()" />
 </template>
