f6218b0593bc57db2e2f85faa394fc491fc65c83
EdwardA10 <EdwardA10@yahoo.com>
2023-04-24 07:45:31 -0700

    Created TreeNode object (pending future edits)

---
+++ Objects/FileNode.hpp
@@ -0,0 +1,47 @@
+#ifndef _FILENODE_H_
+#define _FILENODE_H_
+
+#include <ctime>
+#include <chrono>
+#include <iostream>
+
+class FileNode {
+public:
+	FileNode(char* fn) : //Receive pointer to filename string
+		//Store filename and retrieve timestamp as time since Epoch (Avoid locality time differences)
+		filename(fn), timestamp(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())) {} 
+
+	//Filename string should be dynamically allocated at runtime so memory should be freed when file is removed from Database
+	~FileNode() { 
+	}
+
+	//Return the pointer to the filename
+	char* get_Filename() {
+		return filename;
+	}
+
+	//Return time since last Epoch
+	std::time_t get_Timestamp() {
+		return timestamp;
+	}
+
+	//Print Filename and Date in Local Calendar Format
+	void printFile() {
+		std::cout << "File Name: " << filename
+			<< "\nDate Last Updated: " << std::asctime(std::localtime(&timestamp)) << std::endl;
+	}
+
+	//Change Filename to provided Filename and print status of update
+	//Currently assumes file extension is passed as part of file name string so no parsing occurs here)
+	//Updates timestamp once update completes
+	void updateFilename(char* fn) {
+		filename = fn;
+		timestamp = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+	}
+
+
+private:
+	char* filename;
+	std::time_t timestamp;
+};
+#endif
---
+++ Objects/TreeNode.hpp
@@ -0,0 +1,94 @@
+#ifndef _TREENODE_H_
+#define _TREENODE_H_
+
+#include <vector>
+#include "FileNode.hpp"
+
+class TreeNode {
+  public:
+  //Constructor which creates the tree's root node.
+  TreeNode(FileNode* f) {
+      file = f;
+      filetype = (char*)"root";
+    }
+
+    //Constructor which creates a node in the tree and passes in a filetype (this node to be used as a child of a filetype node)
+    TreeNode(FileNode* f, char* ft) {
+      file = f;
+      filetype = ft;
+    }
+
+    ~TreeNode() {
+    }
+
+    //Returns the file's filetype (ex: png, txt, pdf, etc.)
+    char* get_Filetype() {
+      return filetype;
+    }
+
+    //Determines if a particular file's name and type already exists among the children of a filetype node.
+    bool isDuplicateFile(TreeNode* directory, TreeNode* newFile) {
+      for(int i = 0; i < directory->children.size(); ++i) {
+        if(directory->children.at(i)->file->get_Filename() == newFile->file->get_Filename()) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    //A function which adds a child to a node's children vector
+    void addChild(TreeNode* child) {
+      if(isDuplicateFile(this, child) == true) {
+        std::cout << "ERROR! File of this type and with this filename already exists! Please change filename." << std::endl;
+      }
+      else { this->children.push_back(child); }
+      return;
+    }
+
+    /* Was working on a way to populate the root node with
+    children nodes that represent each file type.
+    A vector of TreeNode* with all supported file types must 
+    be declared as a global variable somewhere.
+    Must complete this some other time...*/
+    void populateRoot(std::vector<TreeNode*> types) {
+      if(this->get_Filetype() != "root") {
+        std::cout << "ERROR! Cannot populate non-root node with children consisting of file types." << std::endl;
+        return;
+      }
+      for(int i = 0; i < types.size(); i++) {
+        //parent->addChild(parent, types.at(i));
+        this->addChild(types.at(i));
+      }
+      return;
+    }
+
+    //Helper function which prints a nodes filetype
+    void printFileType() {
+      std::cout << "File type: " << filetype << std::endl;
+      return;
+    }
+
+    //Helper function which returns all immediate children of a TreeNode
+    void printChildren() {
+      if(children.size() == 0) {
+        std::cout << "ERROR! No children to display!" << std::endl;
+        return;
+      }
+
+      std::cout << "File type: " << filetype << std::endl;
+      for(int i = 0; i < children.size(); ++i) {
+        std::cout << "Printing " << i+1 << "-th child's filename and file type..." << std::endl;
+        std::cout << i+1 << "-th child's filename: " << children.at(i)->file->get_Filename() << std::endl;
+        std::cout << i+1 << "-th child's filetype: " << children.at(i)->get_Filetype() << std::endl;
+      }
+      std::cout << std::endl;
+    }
+
+  private:
+    FileNode* file;
+    char* filetype;
+    std::vector<TreeNode*> children;
+};
+
+
+#endif
