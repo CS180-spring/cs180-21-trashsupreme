47643b7b9255a63e2b584a637477038a52637a83
hscottvo <56327086+hscottvo@users.noreply.github.com>
2023-05-15 11:44:40 -0700

    Converted char pointers to string

--- Database/Objects/FileNode.hpp
+++ Database/Objects/FileNode.hpp
@@ -1,46 +1,83 @@
 #ifndef _FILENODE_H_
 #define _FILENODE_H_
 
-#include <ctime>
+#include "json.hpp"
 #include <iostream>
+#include <ctime>
+#include <chrono>
+#include <cassert>
+#include <unordered_map>
 
-class FileNode {
-public:
-	FileNode(char* fn) : //Receive pointer to filename string
-		//Store filename and retrieve timestamp as time since Epoch (Avoid locality time differences)
-		filename(fn), timestamp(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())) {} 
-	
-	//Filename string should be dynamically allocated at runtime so memory should be freed when file is removed from Database
-	~FileNode() { 
-	}
+using namespace std;
+using json = nlohmann::json;
 
-	//Return the pointer to the filename
-	char* get_Filename() {
-		return filename;
-	}
+namespace file
+{
+  class fileNode
+  {
+  public:
+    // Test Constructor where all variables are declared by user
+    fileNode(std::string fn, std::string type, std::string ID) : filename(fn), filetype(type), docID(ID), timestamp(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())) {}
 
-	//Return time since last Epoch
-	std::time_t get_Timestamp() {
-		return timestamp;
-	}
+    // Main constructor which creates filetype without explicitly declaring it
+    fileNode(std::string fn, std::string ID) : filename(fn), filetype(filename.substr(filename.find(".") + 1)), docID(ID), timestamp(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())) {}
+    ~fileNode() {}
 
-	//Print Filename and Date in Local Calendar Format
-	void printFile() {
-		std::cout << "File Name: " << filename
-			<< "\nDate Last Updated: " << std::asctime(std::localtime(&timestamp)) << std::endl;
-	}
+    std::string get_filename()
+    {
+      return filename;
+    }
 
-	//Change Filename to provided Filename and print status of update
-	//Currently assumes file extension is passed as part of file name string so no parsing occurs here)
-	//Updates timestamp once update completes
-	void updateFilename(char* fn) {
-		filename = fn;
-		timestamp = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-	}
+    std::string get_fileExtension()
+    {
+      std::string substring = this->filename;
+      std::string extension;
+      // extension = strchr(substr, '.');
+      extension = substring.substr(substring.find(".") + 1);
 
+      return extension;
+    }
 
-private:
-	char* filename;
-	std::time_t timestamp;
-};
+    std::string get_docID()
+    {
+      return docID;
+    }
+
+    void updateFilename(std::string fn)
+    {
+      filename = fn;
+      // filename_string = (std::string)filename;
+      timestamp = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    }
+
+    void printFile()
+    {
+      std::cout << "File Name: " << filename
+                << "\nDate Last Updated: " << std::asctime(std::localtime(&timestamp)) << std::endl;
+    }
+
+    std::string filename;
+    // std::string filename_string = (std::string)filename; // overloaded char* to string to be compatible with JSON functions below
+    std::string filetype;
+    // std::string filetype_string = (std::string)filetype; // overloaded char* to string to be compatible with JSON functions below
+    std::string docID;
+    // std::string docID_string = (std::string)docID; // overloaded char* to string to be compatible with JSON functions below
+    std::string path;
+    // std::string path_string = (std::string)path;
+    std::time_t timestamp;
+  };
+  void to_json(json &j, const fileNode &p)
+  {
+    j = json{{"filename", p.filename}, {"filetype", p.filetype}, {"docID", p.docID}, {"timestamp", p.timestamp}};
+  }
+
+  void from_json(const json &j, fileNode &p)
+  {
+    j.at("filename").get_to(p.filename);
+    j.at("filetype").get_to(p.filetype);
+    j.at("docID").get_to(p.docID);
+    j.at("timestamp").get_to(p.timestamp);
+  }
+}
+
 #endif
---
+++ Database/Objects/FileTree.hpp
@@ -0,0 +1,106 @@
+#ifndef __FILETREE_HPP__
+#define __FILETREE_HPP__
+
+#include "FileNode.hpp"
+#include <vector>
+#include <unordered_map>
+#include <cstring>
+
+class FileTree
+{
+public:
+  /*Constructs a FileTree whose name is based on the filetype of the referenced FileNode
+  Example: a file with filename "Words.txt" passed into the constructor will create a FileTree
+  whose name is ".txt" */
+  FileTree(file::fileNode *f)
+  {
+    name = f->get_fileExtension();
+  }
+
+  // Constructor which creates a folder with corresponding folder name and nodeID
+  FileTree(std::string folderName, std::string ID)
+  {
+    name = folderName;
+    nodeID = ID;
+  }
+
+  // Returns the name of the FileTree (ex: ".txt",".pdf", etc.)
+  std::string get_name()
+  {
+    std::string result = "\n";
+    return this->name;
+  }
+
+  // Function which will add a file to the FileTree object's fileMap
+  void filemap_add(std::string docID, file::fileNode *child)
+  {
+    fileMap.insert(std::make_pair(docID, child));
+  }
+
+  // Function which will add a folder to the FileTree object's folderMap
+  void filemap_add(std::string n_ID, FileTree *folder)
+  {
+    folderMap.insert(std::make_pair(n_ID, folder));
+  }
+
+  // Removes file with corresponding key (docID) from the tree
+  void filemap_remove(std::string key)
+  {
+    // If a file with this key exists, then we can properly remove it
+    if (this->find_key(key) != false)
+    {
+      fileMap.erase(key);
+      return;
+    }
+    // Otherwise, we cannot remove a file with a key that is not mapped/does not exist!
+    else
+    {
+      std::cout << "Unable to remove as a file with that key doesn't exist!" << std::endl;
+      return;
+    }
+  }
+
+  /*Helper function which shall output a message confirming
+  whether a file of this filename exists. */
+  void doesFileExist(std::string key)
+  {
+    const auto &keys = fileMap; // auto which will search the fileMap
+    if (keys.find(key) != keys.end())
+    {
+      file::fileNode *mapData = keys.at(key);
+      std::cout << "File " << mapData->get_filename() << " exists!" << std::endl;
+      return;
+    }
+    else
+    {
+      std::cout << "File " << key << " doesn't exist!" << std::endl;
+      return;
+    }
+  }
+
+  /*Helper function which returns a boolean value confirming
+  whether a file with this docID exists. */
+  bool find_key(std::string key)
+  {
+    const auto &keys = fileMap; // auto which will search the fileMap
+    if (keys.find(key) != keys.end())
+    {
+      // file::fileNode* mapData = keys.at(key);
+      // std::cout << "File " << mapData->get_filename() << " exists!" << std::endl;
+      return true;
+    }
+    else
+    {
+      // std::cout << "File " << key << " doesn't exist!" << std::endl;
+      return false;
+    }
+  }
+
+private:
+  std::string name;
+  std::string nodeID; // ID for a folder node
+  std::unordered_map<std::string, file::fileNode *> fileMap;
+  std::unordered_map<std::string, FileTree *> folderMap;
+};
+
+#endif
